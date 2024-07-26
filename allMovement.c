ce77058f3ce6c41c16c3b97636fd35a32951fa4e
DBKorn <70672608+DBKorn@users.noreply.github.com>
2024-07-26 19:31:23 -0400

    Update allMovement.c

--- allMovement.c
+++ allMovement.c
@@ -4,30 +4,6 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-/*
- * wiringPi C library use different GPIO pin number system from BCM pin numberwhich are often used by Python, 
- * you can lookup BCM/wPi/Physical pin relation by following Linux command : gpio readall
- */
-const int enableA = 0;  //left motor speed pin ENA connect to PCA9685 port 0
-const int enableB = 1;  //right motor speed pin ENB connect to PCA9685 port 1
-const int leftSideIn1 = 4;  //Left motor leftSideIn1 connect to wPi pin# 4 (Physical 16,BCM GPIO 23)
-const int leftSideIn2 = 5;  //Left motor leftSideIn2 connect to wPi pin# 5 (Physical 18,BCM GPIO 24)
-const int rightSideIn3 = 2;  //right motor rightSideIn3 connect to wPi pin# 2 (Physical 13,BCM GPIO 27)
-const int rightSideIn4 = 3;  //right motor rightSideIn4 connect to wPi pin# 3 (Physical 15,BCM GPIO 22)
-const int SPEED = 2000;
-
-// initialize  leftSideIn1, leftSideIn2, rightSideIn3, rightSideIn4 
-void setup(){
- pinMode(leftSideIn1,OUTPUT);
- pinMode(leftSideIn2,OUTPUT);
- pinMode(rightSideIn3,OUTPUT);
- pinMode(rightSideIn4,OUTPUT);
- 
- digitalWrite(leftSideIn1,LOW);
- digitalWrite(leftSideIn2,LOW);
- digitalWrite(rightSideIn3,LOW);
- digitalWrite(rightSideIn4,LOW);
-}
 void reverse(int fd,int speed){
     digitalWrite(leftSideIn1,HIGH);
     digitalWrite(leftSideIn2,LOW);
